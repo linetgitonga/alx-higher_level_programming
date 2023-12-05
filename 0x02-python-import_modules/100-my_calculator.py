1bbab24d274598853f5d87c9a2a89edf9e4ef72b
linetgitonga <linetgitonga55@gmail.com>
2023-12-05 15:01:27 -0800

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,19 @@
-from sys import argv
-
-
-from calculator_1 import add, sub, mul, div
-operators = ["+", "-", "*", "/"]
-arg_len = len(argv)
+#!/usr/bin/python3
 
 if __name__ == "__main__":
-    if (arg_len - 1) < 3:
+    
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    elif argv[2] not in operators:
+        sys.exit(1)
+
+    operations = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(operations.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    else:
-        a, b = int(argv[1]), int(argv[3])
-        if argv[2] == "+":
-            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-        elif argv[2] == "-":
-            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-        elif argv[2] == "*":
-            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-        else:
-            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, operations[sys.argv[2]](a, b)))
