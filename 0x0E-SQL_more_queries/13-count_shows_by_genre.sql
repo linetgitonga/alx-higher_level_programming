565c48d87107a203c3e5db401e991e166a327992
linetgitonga <linetgitonga55@gmail.com>
2024-02-15 18:50:51 -0800

    SQL Queries

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,13 @@
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+-- Each record should display: tv_genres.name - number of shows
+-- First column must be called genre
+-- Second column must be called number_of_shows
+-- Results must be sorted in descending order by the number of shows link
+-- The database name will be passed as an argument of the mysql command
+
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id=tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
